f4bc3799a8bb10e356414596434b81fcd5896ab6
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2025-01-15 12:50:40 +0100

    eje1 y eje2 hechos

--- Tema4-Scala/scala3/src/main/scala/EjerciciciosMap/Ejercicio1.scala
+++ Tema4-Scala/scala3/src/main/scala/EjerciciciosMap/Ejercicio1.scala
@@ -1,26 +1,15 @@
+package esp.scala3.app
 package EjerciciciosMap
 
-object Ejercicio1 {
-    val frutas = List("Manzana", "Pera", "Naranja")
-    val precios = List(1, 2, 5)
-    val cantidadCompra = List(2, 4, 10)
+object Ejercicio1 extends App {
 
-    def main(args: Array[String]): Unit = {
-        // 1. Filtrar los índices de las frutas cuyo precio por unidad sea mayor a 3
-        val indicesFiltrados = precios.zipWithIndex.filter(_._1 > 3).map(_._2)
+    val alumnos = List("Samuel", "Lucas", "Roberto")
+    val asignaturas = List("PIA", "BDA", "SAA", "MIA", "SBD")
 
-        // 2. Calcular el precio total para cada índice filtrado
-        val preciosTotales = indicesFiltrados.map { i =>
-            val total = precios(i) * cantidadCompra(i)
-            (frutas(i), total)
-        }
+    val combinaciones = for {
+        alumno <- alumnos
+        asignatura <- asignaturas
+    } yield s"$alumno está inscrito en $asignatura"
 
-        // 3. Generar la lista de mensajes en el formato requerido
-        val mensajes = preciosTotales.map { par =>
-            "Fruta: " + par._1 + ", Total: " + par._2
-        }
-
-        // Imprimir los resultados
-        mensajes.foreach(println)
-    }
+    combinaciones.foreach(println)
 }
--- Tema4-Scala/scala3/src/main/scala/EjerciciciosMap/Ejercicio2.scala
+++ Tema4-Scala/scala3/src/main/scala/EjerciciciosMap/Ejercicio2.scala
@@ -1,26 +1,19 @@
+package esp.scala3.app
 package EjerciciciosMap
 
 object Ejercicio2 {
-    val frutas = List("Manzana", "Pera", "Naranja")
-    val precios = List(1, 2, 5)
-    val cantidadCompra = List(2, 4, 10)
+    val frutas = List("Manzana", "Pera", "Naranja", "Uva")
+    val precios = List(1, 2, 5, 7)
+    val cantidadFruta = List(2, 4, 10, 20)
 
     def main(args: Array[String]): Unit = {
+        val mensajes = frutas.indices
+          .filter(fruta => precios(fruta) > 3)
+          .map(fruta => {
+              val precioTotal = precios(fruta) * cantidadFruta(fruta)
+              s"Fruta: ${frutas(fruta)}, Precio total: $precioTotal"
+          })
 
-        // 1. Filtrar los índices de las frutas cuyo precio por unidad sea mayor a 3
-        val indicesFiltrados = precios.indices.filter(i => precios(i) > 3)
-
-        // 2. Calcular el precio total para cada índice filtrado
-        val preciosTotales = indicesFiltrados.map(i =>
-            (frutas(i), precios(i) * cantidadCompra(i))
-        )
-
-        // 3. Generar la lista de mensajes en el formato requerido
-        val mensajes = preciosTotales.map(par =>
-            "Fruta: " + par._1 + ", Total: " + par._2
-        )
-
-        // Imprimir los resultados
         mensajes.foreach(println)
     }
 }
