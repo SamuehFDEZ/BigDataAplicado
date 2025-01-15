a42e093a7cc74d4ef6bb55207308156c1a4260c6
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2025-01-15 12:35:38 +0100

    fixed intellij scala3 project

---
+++ Tema4-Scala/scala3/src/main/scala/EjerciciciosMap/Ejercicio1.scala
@@ -0,0 +1,26 @@
+package EjerciciciosMap
+
+object Ejercicio1 {
+    val frutas = List("Manzana", "Pera", "Naranja")
+    val precios = List(1, 2, 5)
+    val cantidadCompra = List(2, 4, 10)
+
+    def main(args: Array[String]): Unit = {
+        // 1. Filtrar los índices de las frutas cuyo precio por unidad sea mayor a 3
+        val indicesFiltrados = precios.zipWithIndex.filter(_._1 > 3).map(_._2)
+
+        // 2. Calcular el precio total para cada índice filtrado
+        val preciosTotales = indicesFiltrados.map { i =>
+            val total = precios(i) * cantidadCompra(i)
+            (frutas(i), total)
+        }
+
+        // 3. Generar la lista de mensajes en el formato requerido
+        val mensajes = preciosTotales.map { par =>
+            "Fruta: " + par._1 + ", Total: " + par._2
+        }
+
+        // Imprimir los resultados
+        mensajes.foreach(println)
+    }
+}
---
+++ Tema4-Scala/scala3/src/main/scala/main.scala
@@ -0,0 +1,6 @@
+package esp.scala3.app
+
+@main
+def main(): Unit =
+  println("Hello world!")
+
