49f7d5760a5e9bbc84016a1c1c168b00fb9e2f01
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2025-01-20 20:37:01 +0100

    pdf de proyecto nifi y ejes de scala

---
+++ Tema4-Scala/scala3/src/main/scala/ListasVectores/Vectorizar.scala
@@ -0,0 +1,32 @@
+package esp.scala3.app
+package ListasVectores
+
+import scala.util.Random
+
+object Vectorizar extends App {
+
+  val vector: Vector[Int] = Vector(1, 2, 3)
+  println(vector)
+
+  //vector vs lists
+  val maxRuns = 1000
+  val maxCapacity = 1000000
+
+  def getWriteTime(collection: Seq[Int]): Double = {
+    val r = new Random
+    val times = for {
+      it <- 1 to maxRuns
+    } yield {
+      val currentTime = System.nanoTime()
+      collection.updated(r.nextInt(maxCapacity), r.nextInt)
+      System.nanoTime - currentTime
+    }
+    times.sum * 1.0 / maxRuns
+  }
+
+  val numberLists = (1 to maxCapacity).toList
+  val numberVector = (1 to maxCapacity).toVector
+
+  println(getWriteTime(numberLists))
+  println(getWriteTime(numberVector))
+}
