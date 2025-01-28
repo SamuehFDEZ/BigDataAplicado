58a935931d34516f606a8b7ee0ab1832e06d021e
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2025-01-28 10:14:22 +0100

    option

--- Tema4-Scala/scala3/src/main/scala/Opciones/Option.scala
+++ Tema4-Scala/scala3/src/main/scala/Opciones/Option.scala
@@ -1,6 +1,31 @@
 package esp.scala3.app
 package Opciones
 
-object Option {
-  
+object Option extends App {
+    val sistema: Map[String, Map[String, String]] = Map(
+      "1" -> Map("nombre" -> "Samuel", "saldo" -> "1300"),
+      "2" -> Map("nombre" -> "Roberto", "saldo" -> "1300"),
+      "3" -> Map("nombre" -> "Lucas")
+    )
+
+    def obtenerUsuario(id: String): Unit = {
+      sistema.get(id) match {
+        case Some(infoUsuario) =>
+          val nombre = infoUsuario.getOrElse("nombre", "Desconocido")
+          infoUsuario.get("saldo") match {
+            case Some(saldo) =>
+              val saldoNumerico = saldo.toDouble
+              val nuevoSaldo = saldoNumerico * 1.10
+              println(s"(Id: $id, Nombre: $nombre, Nuevo Saldo: $nuevoSaldo)")
+            case None =>
+              println(s"(Id: $id, Nombre: $nombre) ---> OJO: NO TIENE SALDO")
+          }
+        case None =>
+      }
+    }
+
+    obtenerUsuario("1")
+    obtenerUsuario("2")
+    obtenerUsuario("3")
+    obtenerUsuario("4")
 }
