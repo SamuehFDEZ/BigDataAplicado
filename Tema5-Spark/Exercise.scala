490aaf25ba18336520faedc8764ef73d63abd637
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2025-03-06 18:06:22 +0100

    ejercicio hecho

--- Tema5-Spark/Exercise.scala
+++ Tema5-Spark/Exercise.scala
@@ -1,8 +1,9 @@
 // Databricks notebook source
-//Analizar los salarios de sus empleados en diferentes departamentso para obtener información clave sobre la distribución de los sueldos.
-
+// MAGIC %md
+// MAGIC # Analizar los salarios de sus empleados en diferentes departamentso para obtener información clave sobre la distribución de los sueldos.
 
 // COMMAND ----------
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
@@ -12,12 +13,24 @@ import org.apache.spark.sql.Row
 val spark = SparkSession.builder.appName("AnalisisSalarios").getOrCreate()
 
 // Definir el esquema de los datos
-val schema =
+val schema = StructType(Array(
+  StructField("id", IntegerType),
+  StructField("Nombre del empleado", StringType),
+  StructField("Departamento", StringType),
+  StructField("Salario", DoubleType),
+))
 
 
 // COMMAND ----------
+
 // Datos de empleados
-val datos =
+val datos = Seq(
+  Row(1,"Ana Soria Serrano", "Ventas", 3000.0),
+  Row(2,"Luis García Pérez", "Administración", 2500.0),
+  Row(3,"Roberto Sánchez Sánchez", "Ventas", 3500.0),
+  Row(4,"Pedro Serrano Martínez", "RRHH", 3200.0),
+  Row(5,"Alejandro Giménez Sánchez", "Administración", 2700.0)
+)
 
 
 // COMMAND ----------
@@ -28,26 +41,34 @@ val df = spark.createDataFrame(spark.sparkContext.parallelize(datos), schema)
 df.show()
 
 // COMMAND ----------
-// Agrupar por departamento y calcular salario promedio
-val dfAgrupado = 
 
+// Agrupar por departamento y calcular salario promedio
+val dfAgrupado = df
+  .groupBy("Departamento")
+  .agg(avg("Salario").as("Media_Salario"))
+  
 // Mostrar los resultados
 dfAgrupado.show()
 
 // COMMAND ----------
+
 // Ordenar de mayor a menor salario promedio
-val dfOrdenado =
+val dfOrdenado = dfAgrupado.orderBy(desc("Media_Salario"))
 
 // Mostrar resultados ordenados
 dfOrdenado.show()
 
 // COMMAND ----------
-// Crear vista temporal para consultas SQL
+
+// Crear vista temporal para consultas SQL 
+// Antes estaba como dfAgrupado esto es un problema porque Media_Salario ya está aplicado
+df.createOrReplaceTempView("AnalisisSalarios")
 
 // COMMAND ----------
+
 // Consulta SQL en Spark
-val dfSQL = spark.sql(...)
+// Ejecutar una consulta SQL que devuelva el salario promedio por departamento, ordenado de mayor a menor
+val dfSQL = spark.sql("SELECT Departamento, avg(Salario) AS Media_Salarios FROM AnalisisSalarios GROUP BY Departamento ORDER BY Media_Salarios DESC")
 // Mostrar resultados de SQL
 dfSQL.show()
-
-
+// SELECT Departamento, avg('Salario') as Media_Salarios FROM Salarios GROUP BY Departamento ORDER BY Media_Salarios DESC
